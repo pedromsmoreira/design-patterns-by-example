dafe17b81d07caafa07db872d01843f3e1f7502e
Pedro Moreira <pedro.ms.moreira51@gmail.com>
2017-08-27 19:20:25 +0100

    Add console apps for Creation Patterns

---
+++ src/FactoryMethodPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoryMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/SingletonPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
